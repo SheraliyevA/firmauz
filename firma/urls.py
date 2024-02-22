9f9486f18a1748c0c8ac7f529051aa11c678c782
sheraliyev <sheraliyev1310@gmail.com>
2024-02-22 18:15:52 +0500

    firma.url commit

--- firma/urls.py
+++ firma/urls.py
@@ -24,17 +24,17 @@ urlpatterns=[
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('api/tutorials',Mahsulot_view.as_view()),
-    path('api/tutorials/<int:id>',MahsulotDetail.as_view()),
-    path('api/ish',Ishturi.as_view()),
-    path('api/ish/<int:id>',IshDetail.as_view()),
+    path('mahsulot/',Mahsulot_view.as_view()),
+    path('mahsulot/<int:id>',MahsulotDetail.as_view()),
+    path('ish_turi/',Ishturi.as_view()),
+    path('ish_turi/<int:id>',IshDetail.as_view()),
     path('api/xodim',Xodim_view.as_view()),
     path('api/xodim/<int:id>',XodimDetail.as_view()),
-    path('api/xato',Xato_view.as_view()),
-    path('api/xato/<int:id>',XatoDetail.as_view()),
-    path('api/missed',Missed_view.as_view()),
-    path('api/missed/<int:id>',MissedDetail.as_view()),
-    path('api/bulim',Bulim_View.as_view()),
-    path('api/bulim/<int:id>',Bulim_Detail.as_view())
+    path('xatolar/',Xato_view.as_view()),
+    path('xatolar/<int:id>',XatoDetail.as_view()),
+    path('missed',Missed_view.as_view()),
+    path('missed/<int:id>',MissedDetail.as_view()),
+    path('bulim/',Bulim_View.as_view()),
+    path('bulim/<int:id>',Bulim_Detail.as_view())
     
 ]
